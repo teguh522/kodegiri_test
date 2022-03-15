39a195519e38a8f15932f81b7ec2e46bb6bb6df7
Teguh Firmansyah <teguhf522@gmail.com>
2022-03-15 14:51:11 +0700

    rest api finish

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -3,11 +3,11 @@ const authenticateJWT = require('../middleware/jwtVerify')
 const db = require('../models')
 
 
-routes.route('/').get(authenticateJWT, async (req, res) => {
+routes.route('/list/:limit/:offset').get(authenticateJWT, async (req, res) => {
     try {
         const events = await db.Event.findAll({
-            limit: 2,
-            offset: 0
+            limit: req.params.limit,
+            offset: req.params.offset
         })
         res.json({
             code: 200,
@@ -23,9 +23,19 @@ routes.route('/').get(authenticateJWT, async (req, res) => {
             message: "Internal Server Error"
         })
     }
-}).post(authenticateJWT, async (req, res) => {
+})
+
+routes.route('/').post(authenticateJWT, async (req, res) => {
     try {
         const create = await db.Event.create(req.body)
+        res.status(201).json({
+            code: 201,
+            message: "success",
+            data: {
+                id: create.id,
+                event: create.eventname
+            }
+        })
     } catch (error) {
         console.log(error);
         res.status(500).json({
@@ -35,5 +45,63 @@ routes.route('/').get(authenticateJWT, async (req, res) => {
     }
 })
 
+routes.route('/:id').get(authenticateJWT, async (req, res) => {
+    try {
+        const event = await db.Event.findOne({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.json({
+            code: 200,
+            message: 'Success',
+            data: {
+                event
+            }
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            code: 500,
+            message: "Internal Server Error"
+        })
+    }
+}).delete(authenticateJWT, async (req, res) => {
+    try {
+        const deletedata = await db.Event.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.json({
+            code: 200,
+            message: 'success',
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            code: 500,
+            message: "Internal Server Error"
+        })
+    }
+}).put(authenticateJWT, async (req, res) => {
+    try {
+        const updatedata = await db.Event.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        })
+        res.json({
+            code: 200,
+            message: 'success'
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            code: 500,
+            message: "Internal Server Error"
+        })
+    }
+})
 
 module.exports = routes
