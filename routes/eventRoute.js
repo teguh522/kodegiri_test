7d090d9a015ff05cee297c785dc340ec0f991ec7
Teguh Firmansyah <teguhf522@gmail.com>
2022-03-15 15:26:33 +0700

    update file finish

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -1,7 +1,30 @@
 const routes = require('express').Router()
 const authenticateJWT = require('../middleware/jwtVerify')
 const db = require('../models')
+const multer = require('multer');
+const path = require('path')
+const fs = require('fs')
 
+const imageStorage = multer.diskStorage({
+    destination: 'public/images',
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '_' + Date.now()
+            + path.extname(file.originalname))
+    }
+});
+
+const imageUpload = multer({
+    storage: imageStorage,
+    limits: {
+        fileSize: 10000000 // 10 MB
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
+            return cb(new Error('Please upload a Image'))
+        }
+        cb(undefined, true)
+    }
+})
 
 routes.route('/list/:limit/:offset').get(authenticateJWT, async (req, res) => {
     try {
@@ -25,9 +48,9 @@ routes.route('/list/:limit/:offset').get(authenticateJWT, async (req, res) => {
     }
 })
 
-routes.route('/').post(authenticateJWT, async (req, res) => {
+routes.route('/').post(authenticateJWT, imageUpload.single('image'), async (req, res) => {
     try {
-        const create = await db.Event.create(req.body)
+        const create = await db.Event.create({ ...req.body, image: req.file.filename })
         res.status(201).json({
             code: 201,
             message: "success",
@@ -45,63 +68,73 @@ routes.route('/').post(authenticateJWT, async (req, res) => {
     }
 })
 
-routes.route('/:id').get(authenticateJWT, async (req, res) => {
-    try {
-        const event = await db.Event.findOne({
-            where: {
-                id: req.params.id
-            }
-        })
-        res.json({
-            code: 200,
-            message: 'Success',
-            data: {
-                event
-            }
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            code: 500,
-            message: "Internal Server Error"
-        })
-    }
-}).delete(authenticateJWT, async (req, res) => {
-    try {
-        const deletedata = await db.Event.destroy({
-            where: {
-                id: req.params.id
-            }
-        })
-        res.json({
-            code: 200,
-            message: 'success',
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            code: 500,
-            message: "Internal Server Error"
-        })
-    }
-}).put(authenticateJWT, async (req, res) => {
-    try {
-        const updatedata = await db.Event.update(req.body, {
-            where: {
-                id: req.params.id
-            }
-        })
-        res.json({
-            code: 200,
-            message: 'success'
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            code: 500,
-            message: "Internal Server Error"
-        })
-    }
-})
+routes.route('/:id')
+    .get(authenticateJWT, async (req, res) => {
+        try {
+            const event = await db.Event.findOne({
+                where: {
+                    id: req.params.id
+                }
+            })
+            res.json({
+                code: 200,
+                message: 'Success',
+                data: {
+                    event
+                }
+            })
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                code: 500,
+                message: "Internal Server Error"
+            })
+        }
+    }).delete(authenticateJWT, async (req, res) => {
+        try {
+            const findimage = await db.Event.findOne({
+                where: {
+                    id: req.params.id
+                }
+            })
+
+            fs.unlink(`public/images/${findimage.image}`, (err) => {
+
+            })
+            const deletedata = await db.Event.destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+            res.json({
+                code: 200,
+                message: 'success',
+            })
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                code: 500,
+                message: "Internal Server Error"
+            })
+        }
+    }).put(authenticateJWT, async (req, res) => {
+        try {
+            const updatedata = await db.Event.update(req.body, {
+                where: {
+                    id: req.params.id
+                }
+            })
+            res.json({
+                code: 200,
+                message: 'success'
+            })
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                code: 500,
+                message: "Internal Server Error"
+            })
+        }
+    })
 
 module.exports = routes
